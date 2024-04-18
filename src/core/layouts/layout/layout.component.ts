5dc3d968d7fcabcb829d7c93a0817fa822685ef2
amirh <amirh@a.com>
2024-04-19 00:19:54 +0330

    add layout component

---
+++ src/core/layouts/layout/layout.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+  @Input() layout !: string;
+
+}
